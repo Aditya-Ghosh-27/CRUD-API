f54ac51b7212ecb3cc30a3dedac1ce2189fa259f
Aditya Ghosh <adi27.ghosh@gmail.com>
2023-12-31 00:45:07 +0530

    mock users set commit

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 import express from 'express';
 import bodyParser from 'body-parser'; // This allows us to take in incoming POST request bodies
 
+import usersRoutes from './routes/user.js';
+
 const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json()); // bodyParser Middleware
 
-app.get('/', (req, res) => {
-    res.send("Hello from Homepage!");
-})
+app.use('/users', usersRoutes);
 
 app.listen(PORT, () => console.log(`Server running on PORT: http://localhost:${PORT}`));
--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,28 @@
 import express from 'express';
 
 const router = express.Router(); // A router coming from express.Router
+
+const users = [
+    {
+        firstname: "Aditya",
+        lastname: "Ghosh",
+        age: 20
+    },
+    {
+        firstname: "Anindo",
+        lastname: "Chowdhury",
+        age: 19
+    },
+    {
+        firstname: "Sk. Firdous",
+        lastname: "Rahaman",
+        age: 20
+    }
+];
+
+router.get('/', (req, res) => {
+    console.log(users);
+    res.send("Hello");
+});
+
+export default router;
